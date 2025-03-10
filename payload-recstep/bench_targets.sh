a737726f7100869ef6e9c33f2e11a06c16fe4fd5
Srinag <=>
2025-03-10 15:45:26 -0500

    Added DOOP targetz

--- payload-recstep/bench_targets.sh
+++ payload-recstep/bench_targets.sh
@@ -1,26 +1,30 @@
 targets=(
 	# Fast
-	"xalan doop xalan isType EScSi"
-	"batik doop batik isType EScSi"
-	# "sg gt G10K-0.001 Sg DERScSi"
+	"xalan doop xalan SubtypeOf EScSi"
+	"batik doop batik SubtypeOf EScSi"
+	"zxing doop zxing SubtypeOf EScSi"
+	"biojava doop biojava SubtypeOf EScSi"
+	"eclipse doop eclipse SubtypeOf EScSi"
+
+	"sg gt G10K-0.001 Sg DERScSi"
 	"sg gt G5K-0.001 Sg DERScSi"
 
-	# "tc gt G10K-0.001 Tc DERScSi"
-	# "tc gt G5K-0.001 Tc DERScSi"
+	"tc gt G10K-0.001 Tc DERScSi"
+	"tc gt G5K-0.001 Tc DERScSi"
 
-	# "reach ga livejournal Reach DERScSi"
-	# "reach ga orkut Reach DERScSi"
+	"reach ga livejournal Reach DERScSi"
+	"reach ga orkut Reach DERScSi"
 
-	# "andersen andersen 500000 PointsTo DERScSi"
+	"andersen andersen 500000 PointsTo DERScSi"
 
-	# "csda csda httpd Null DERScSi"
-	# "csda csda linux Null DERScSi"
-	# "csda csda postgresql Null DERScSi"
-	# "cspa cspa httpd ValueFlow DERScSi"
-	# "cspa cspa linux ValueFlow DERScSi"
-	# "cspa cspa postgresql ValueFlow DERScSi"
+	"csda csda httpd Null DERScSi"
+	"csda csda linux Null DERScSi"
+	"csda csda postgresql Null DERScSi"
+	"cspa cspa httpd ValueFlow DERScSi"
+	"cspa cspa linux ValueFlow DERScSi"
+	"cspa cspa postgresql ValueFlow DERScSi"
 
-	# "galen misc galen OutP DEScSi"
+	"galen misc galen OutP DEScSi"
 
 	# Slow
 	# "sg gt G10K-0.01 Sg DERScSi"
@@ -35,5 +39,5 @@ targets=(
 	# "tc gt G80K-0.001 Tc DERScSi"
 )
 
-workers=(64)
-# workers=(64 4)
+# workers=(64)
+workers=(64 4)
